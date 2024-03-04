d9202b0ebc225f390435692e82fb0b7e41fc3aa5
Paramesh Kamarthi <kamarthiparamesh@gmail.com>
2024-03-04 16:50:34 +0530

    updated pex

--- growclub/affinidi-login.js
+++ growclub/affinidi-login.js
@@ -8,9 +8,9 @@ function InitAffinidiLoginUI() {
     </div>
   </div>`;
 
-//   html += `<p style="margin:2rem;font-size:20px;text-align:center;">
-//   <b id="subtextArea">Or</b>
-// </p>`
+  //   html += `<p style="margin:2rem;font-size:20px;text-align:center;">
+  //   <b id="subtextArea">Or</b>
+  // </p>`
 
   var blocks = document.querySelectorAll('#password-modal-heading,.account-register-title,#customer_login_box,#customer_login,#customer_loginlogin-sidebar,#customer_createlogin-sidebar,#create_customer');
 
@@ -36,9 +36,40 @@ function Login_Click() {
   else {
     checkout_url = "/account";
   }
+
+  var cartValue = getCookie("cart");
+  if (typeof cartValue !== 'undefined') {
+    createCookie("cart", cartValue, 15);
+  }
+
   location.href = "https://store.xecurify.com/moas/broker/login/shopify/" + shop_name + "/account?redirect_endpoint=" + checkout_url;
+
+
 }
 
 document.addEventListener("DOMContentLoaded", function (event) {
   InitAffinidiLoginUI();
+
 });
+
+
+function getCookie(cookieName) {
+  let cookie = {};
+  document.cookie.split(";").forEach(function (el) {
+    let [key, value] = el.split("=");
+    cookie[key.trim()] = value;
+  })
+  return cookie[cookieName];
+}
+
+function createCookie(name, value, days) {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toGMTString();
+  }
+  else {
+    expires = "";
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
+}
